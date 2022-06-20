0def590a7a401254f3184dc83af43b4b4768e430
K41S4 <70706708+K41S4@users.noreply.github.com>
2022-06-20 23:02:02 +0300

    Part 3 (Razor Views) Task 2 - ViewComponents

---
+++ ExploreNorthwind/Models/BreadcrumbsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreNorthwind.Models
+{
+    public class BreadcrumbsViewModel
+    {
+        public string SectionAction { get; set; }
+        public string SectionName { get; set; }
+        public string SubsectionAction { get; set; }
+        public string SubsectionName { get; set; }
+    }
+}
---
+++ ExploreNorthwind/ViewComponents/BreadcrumbsViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using ExploreNorthwind.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ExploreNorthwind.ViewComponents
+{
+    public class BreadcrumbsViewComponent: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var result = new BreadcrumbsViewModel();
+
+            JObject obj = JObject.Parse(File.ReadAllText("actionNames.json"));
+
+            var section = HttpContext.Request.RouteValues["controller"].ToString();
+            var subsection = HttpContext.Request.RouteValues["action"].ToString();
+
+            result.SectionAction = section;
+            result.SectionName = (string)obj[section] ?? section;
+            if (subsection != "Index")
+            {
+                result.SubsectionAction = subsection;
+                result.SubsectionName = (string)obj[subsection] ?? subsection;
+            }
+
+            return View(result);
+        }
+    }
+}
