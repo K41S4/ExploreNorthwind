1f31cfa18c0d6d170723cd657d4f6ed245286741
K41S4 <70706708+K41S4@users.noreply.github.com>
2022-07-02 20:11:04 +0300

    Part 4 (WebAPI) Task 1: Base API Controllers

---
+++ ExploreNorthwind/ControllersAPI/CategoriesAPIController.cs
@@ -0,0 +1,30 @@
+﻿using ExploreNorthwind.Models;
+using ExploreNorthwindDataAccess.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace ExploreNorthwind.ControllersAPI
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class CategoriesAPIController : Controller
+    {
+        private ICategoriesRepository categoriesRepo { get; }
+        public CategoriesAPIController(ICategoriesRepository categoriesRepo)
+        {
+            this.categoriesRepo = categoriesRepo;
+        }
+
+        public List<CategoryDTO> Get()
+        {
+            var dataCategories = categoriesRepo.GetAll();
+            var dtoCategories = new List<CategoryDTO>();
+            foreach (var item in dataCategories)
+            {
+                dtoCategories.Add(new CategoryDTO(item));
+            }
+            return dtoCategories;
+        }
+    }
+}
---
+++ ExploreNorthwind/ControllersAPI/ProductsAPIController.cs
@@ -0,0 +1,34 @@
+﻿using ExploreNorthwind.ConfigurationOptions;
+using ExploreNorthwind.Models;
+using ExploreNorthwindDataAccess.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+
+namespace ExploreNorthwind.ControllersAPI
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class ProductsAPIController : ControllerBase
+    {
+        private IProductsRepository productsRepo { get; }
+        private ExploreNorthwindOptions options { get; set; }
+        public ProductsAPIController(IProductsRepository productsRepo, IOptionsSnapshot<ExploreNorthwindOptions> options)
+        {
+            this.productsRepo = productsRepo;
+            this.options = options.Value;
+        }
+
+        public List<ProductDTO> Get()
+        {
+            var dataProducts = productsRepo.Get(options.ProductsMaxCount);
+            var dtoProducts = new List<ProductDTO>();
+            foreach (var item in dataProducts)
+            {
+                dtoProducts.Add(new ProductDTO(item));
+            }
+            return dtoProducts;
+        }
+    }
+}
